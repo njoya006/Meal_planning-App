6874a7656250bb6b045e65b9a9a3823768b7f386
Njoya006 <njoyaperfect06@gmail.com>
2025-08-22 08:01:35 +0100

    smo

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,9 @@
-
 from rest_framework.routers import DefaultRouter
 from django.urls import path
 from .views import RecipeViewSet, IngredientViewSet, RecipeRatingViewSet, RecipeLikeViewSet, RecipeCommentViewSet, RecipeReviewsView
 
 router = DefaultRouter()
-router.register(r'', RecipeViewSet)  # Register at root for /api/recipes/
+router.register(r'', RecipeViewSet, basename='recipe')  # Register at root for /api/recipes/ (explicit basename)
 router.register(r'ingredients', IngredientViewSet)
 router.register(r'ratings', RecipeRatingViewSet)
 router.register(r'likes', RecipeLikeViewSet)
