e4021b4c9dacb0be424e5ebfa6ebcacde4f6e990
Njoya006 <njoyaperfect06@gmail.com>
2025-10-02 07:16:48 +0100

    Remove .env from repo history

--- meal_project/settings.py
+++ meal_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 import os
 
 # Load environment variables from .env file
